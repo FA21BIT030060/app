42132160801e449f1dbbdd3b11e7b6ed5b5e7798
hamza <hanzalamir2255@gmail.com>
2025-03-04 06:27:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello, my name is Zeeshan and <span style='color:blue;'>Hamza</span> and this is my app!</h1>"
+    return "<h1>Hello, my name is hamza and <span style='color:blue;'>zeeshan</span> and this is my app!</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
